24074e5ae71dd0034364bb3e83407374c8e28661
kauaanrs <kauaanrs16@gmail.com>
2026-02-24 21:24:46 -0300

    Exercicio de exemplo finalizado

--- Aula 24-02/exercicio1.php
+++ Aula 24-02/exercicio1.php
@@ -22,6 +22,15 @@
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
+        <?php
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+                $num1 = $_POST['num1'];
+                $num2 = $_POST['num2'];
+                $soma = $num1 + $num2;
+                echo "A soma é: $soma";
+            }
+        ?>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
 </body>
