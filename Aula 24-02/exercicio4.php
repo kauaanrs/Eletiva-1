d3192458a3ba4d300986c2e56dbc22ef60845dfc
kauaanrs <kauaanrs16@gmail.com>
2026-02-24 21:43:23 -0300

    Exercicio 4 concluido

---
+++ Aula 24-02/exercicio4.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercicio 4</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="num1" class="form-label">Informe o primeiro número: </label>
+                <input type="number" id="num1" name="num1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="num2" class="form-label">Informe o segundo número: </label>
+                <input type="number" id="num2" name="num2" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $num1 = $_POST['num1'];
+                $num2 = $_POST['num2'];
+                if($num2 == 0){
+                    echo "Divisão por 0. Erro !";
+                }
+                else{
+                    $div = $num1 / $num2;
+                    echo "A divisão é: $div";
+                }
+            }
+
+        ?>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
