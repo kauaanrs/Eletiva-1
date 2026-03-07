aa3735f3c6a95e0ce694a21142beca89c76ac853
kauaanrs <kauaanrs16@gmail.com>
2026-03-06 21:59:49 -0300

    exercicio 3 concluido

--- Aula 03-03/exercicio2.php
+++ Aula 03-03/exercicio2.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio 1</title>
+    <title>Exercicio 2</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Exercicio 1</h1>
+        <h1>Exercicio 2</h1>
         <form method="post">
             <div class="mb-3">
                 <label for="valor1" class="form-label">Digite o primeiro valor da soma: </label>
