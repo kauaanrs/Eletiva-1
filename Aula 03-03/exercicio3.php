f18ff4154a410f6d78a12d357485d1c855265593
kauaanrs <kauaanrs16@gmail.com>
2026-03-06 21:59:56 -0300

    exercicio 3

--- Aula 03-03/exercicio3.php
+++ Aula 03-03/exercicio3.php
@@ -14,14 +14,33 @@
         <form method="post">
             <div class="mb-3">
                 <label for="valorA" class="form-label">Digite um valor para A: </label>
-                <input type="number" id="valorA" name="valorA" class="form-control" required="">
+                <input type="number" step="any" id="valorA" name="valorA" class="form-control" required="">
             </div>
             <div class="mb-3">
                 <label for="valorB" class="form-label">Valor para B: </label>
-                <input type="number" id="valorB" name="valorB" class="form-control" required="">
+                <input type="number" step="any" id="valorB" name="valorB" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                $valorA = $_POST["valorA"];
+                $valorB = $_POST["valorB"];
+
+                if($valorA < $valorB)
+                    echo "$valorA / $valorB";
+                
+                elseif($valorA > $valorB)
+                    echo "$valorB / $valorA";
+                
+                else
+                    echo "Números iguais: $valorA";
+            }
+        
+        ?>
+
+
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
 </body>
